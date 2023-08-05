cc7efc9746c6ccf611844b0cef0b9b950f60416c
GitEagleY <ch0tati@proton.me>
2023-08-05 14:10:35 +0300

    Revert "maked testing for forms and improved some form funcs"
    This reverts commit 57c4a09fb67326a3b1269ef7ce97fd1dfd4a7cee.

--- internal/Handlers/handlers.go
+++ internal/Handlers/handlers.go
@@ -84,7 +84,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	//	form.Has("first_name", r)
 
 	form.Requied("first_name", "last_name", "email", "phone")
-	form.MinLength("first_name", 3)
+	form.MinLenghth("first_name", 3, r)
 	form.IsEmail("email")
 
 	if !form.Valid() {
--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,6 +2,7 @@ package forms
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"strings"
 
@@ -32,22 +33,21 @@ func (f *Form) Requied(fields ...string) {
 }
 
 // checks if form field is in post and not empty
-func (f *Form) Has(field string) bool {
-	x := f.Get(field)
+func (f *Form) Has(field string, r *http.Request) bool {
+	x := r.Form.Get(field)
 	if x == "" {
 		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true
 }
-
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
 }
 
 // checks for string minimum length
-func (f *Form) MinLength(field string, length int) bool {
-	x := f.Get(field)
+func (f *Form) MinLenghth(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
 	if len((x)) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
