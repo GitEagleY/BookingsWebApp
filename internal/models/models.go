b3627170f2b33d641b57172a6af93551679f599b
GitEagleY <ch0tati@proton.me>
2023-08-11 00:14:38 +0300

    setted up models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,8 +1,62 @@
 package models
 
+import "time"
+
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+type Users struct {
+	ID         int
+	FirstName  string
+	LastName   string
+	Email      string
+	Password   string
+	AcessLevel int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+type Rooms struct {
+	ID        int
+	Rooms     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	RoomID    int
+	StartDate time.Time
+	EndDate   time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// room restriction model
+type RoomRestrictions struct {
+	ID            int
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	StartDate     time.Time
+	EndDate       time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservations  Reservations
+	Restriction   Restrictions
 }
