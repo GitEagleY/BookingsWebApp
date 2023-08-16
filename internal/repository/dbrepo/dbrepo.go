7f05cc9db4249fc2d97d329146f3a59b37530004
GitEagleY <ch0tati@proton.me>
2023-08-16 14:59:45 +0300

    writed many tests

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -12,11 +12,21 @@ type postgresDBRepo struct {
 	App *config.AppConfig // App contains the application's configuration settings.
 	DB  *sql.DB           // DB is the PostgreSQL database connection.
 }
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
 
 // NewPostgresRepo creates and returns a new instance of postgresDBRepo.
 func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
 	return &postgresDBRepo{
 		App: a,    // Initialize the App field with the provided AppConfig.
 		DB:  conn, // Initialize the DB field with the provided PostgreSQL database connection.
+	}
+}
+func NewTestingRepo(a *config.AppConfig) repository.DatabaseRepo {
+	return &testDBRepo{
+		App: a, // Initialize the App field with the provided AppConfig.
+		//DB:  conn, // Initialize the DB field with the provided PostgreSQL database connection.
 	}
 }
---
+++ internal/repository/dbrepo/test-repo.go
@@ -0,0 +1,55 @@
+package dbrepo
+
+import (
+	"errors"
+	"time"
+
+	"github.com/GitEagleY/BookingsWebApp/internal/models"
+)
+
+// AllUsers returns a boolean value (always true in this case).
+func (m *testDBRepo) AllUsers() bool {
+	return true
+}
+
+// InsertReservations inserts a new reservation into the database and returns the new ID.
+func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
+	if res.RoomID == 2 {
+		return 0, errors.New("some error")
+	}
+
+	return 1, nil
+}
+
+// InsertRoomRestrictions inserts room restrictions into the database.
+func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	if r.RoomID == 1000 {
+		return errors.New("some error")
+	}
+	return nil
+}
+
+// SearchAvailabilityDatesByRoomsID checks if a room is available within a specified time range.
+func (m *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+
+	return false, nil
+}
+
+// SearchAvailabilityForAllRooms searches for available rooms within a specified time range.
+func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+
+	var rooms []models.Room
+
+	return rooms, nil
+}
+
+// GetRoomByID gets a room by id
+func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
+	var room models.Room
+
+	if id > 2 {
+		return room, errors.New("some erorr")
+	}
+
+	return room, nil
+}
